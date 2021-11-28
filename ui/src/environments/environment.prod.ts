d1fe9683019ba490f043d8aa727ce2ce20460949
Wick <wickerdj@gmail.com>
2021-11-27 21:44:52 -0600

    making sure there is basic enviornment file

---
+++ ui/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true
+  callback: 'set after git ingore is setup',
+  domain: 'set after git ingore is setupm',
+  clientId: 'set after git ingore is setup',
+  audience: 'set after git ingore is setup'
+};
