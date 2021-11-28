b956d7e2f0b8cde841ee53412703c26d187b0379
Wick <wickerdj@gmail.com>
2021-11-27 21:48:32 -0600

    still playing with git ignore

--- ui/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true
-  callback: 'set after git ingore is setup',
-  domain: 'set after git ingore is setupm',
-  clientId: 'set after git ingore is setup',
-  audience: 'set after git ingore is setup'
-};
