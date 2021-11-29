4f4bed7a4093520f4ce7bf89a6e782c1fd3022d2
Wick <wickerdj@gmail.com>
2021-11-28 18:39:54 -0600

    added auth0 package, auth service, callback component, and token interceptor

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {TokenInterceptor} from './token.interceptor';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { TodoComponent } from './todo/todo.component';
+import { CallbackComponent } from './callback/callback.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    TodoComponent
+    TodoComponent,
+    CallbackComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: TokenInterceptor,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
